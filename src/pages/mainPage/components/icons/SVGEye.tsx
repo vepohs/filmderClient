b3478ddee2576240165f771ab2ead0b85ce5778e
Voiturier Matthias <159275303+La227136@users.noreply.github.com>
2024-12-15 16:43:10 +0100

    SVG error répare

--- src/pages/mainPage/components/icons/SVGEye.tsx
+++ src/pages/mainPage/components/icons/SVGEye.tsx
@@ -6,7 +6,7 @@ export function SVGEye({onClick}: Props) {
 
     return (
         <svg onClick={onClick} className='svgEye' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g id="SVGRepo_tracerCarrier"></g>
             <g id="SVGRepo_iconCarrier">
                 <path
