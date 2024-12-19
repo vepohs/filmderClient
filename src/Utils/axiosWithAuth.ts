667284581bff229621d837e8dc04b33ffe7a544f
Fastops <la227136@student.helha.be>
2024-12-19 16:23:16 +0100

    Le swipe quand access token est morte ne sont plus abandonné on refresh et on envoie la requete au back

--- src/Utils/axiosWithAuth.ts
+++ src/Utils/axiosWithAuth.ts
@@ -6,18 +6,51 @@ const axiosWithAuth = axios.create({
     baseURL: `${API_BASE_URL}/api`,  // Point de départ pour tt les requêtes
 });
 
-// Interceptor de requête - pour ajouter le token à chaque requête sortante
 axiosWithAuth.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('accessToken');
+    async config => {
+        let token = localStorage.getItem('accessToken');
 
         if (token) {
+            const { exp } = decodeJwt(token);
+            const nowInSeconds = Math.floor(Date.now() / 1000);
+            const timeLeft = exp - nowInSeconds;
+            console.log(timeLeft)
+            // Si le token expire dans moins de 60 secondes, on le rafraîchit
+            if (timeLeft < 60) {
+                const newAccessToken = await getNewAccessToken();
+                localStorage.setItem('accessToken', newAccessToken);
+                token = newAccessToken; // On utilise le nouveau token
+            }
+
+            // Ajout du token (mis à jour si nécessaire) aux headers
             config.headers.Authorization = `Bearer ${token}`;
         }
+
         return config;
     },
     error => Promise.reject(error)
 );
+
+function decodeJwt (token : string) {
+    // Découpage du token en fonction du séparateur '.'
+    // Un JWT (JSON Web Token) est composé de trois parties séparées par un point :
+    // 1. Header (avant le premier '.')
+    // 2. Payload (entre le premier et le deuxième '.')
+    // 3. Signature (après le deuxième '.')
+    // Le token.split('.')[1] récupère la deuxième partie du JWT, c’est-à-dire le payload encodé en Base64.
+    const base64Url = token.split('.')[1];
+
+    // Les tokens JWT utilisent un encodage Base64URL, qui est une variante du Base64 standard.
+    // Dans le Base64URL, le caractère '-' remplace '+', et '_' remplace '/'
+    // afin de rendre l’encodage sûr pour les URL (pas de caractères spéciaux de base64 classiques).
+    // Ici, on remplace '-' par '+' et '_' par '/' pour rétablir le format standard Base64.
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+
+    // window.atob() est une fonction du navigateur qui décode une chaîne en Base64 standard en texte brut.
+    // On utilise ensuite JSON.parse() pour convertir cette chaîne de texte, qui contient un objet JSON
+    // (le payload du JWT), en un objet JavaScript.
+    return JSON.parse(window.atob(base64));
+}
 
 // Interceptor de réponse - pour rafraîchir le token en cas de 401
 axiosWithAuth.interceptors.response.use(
